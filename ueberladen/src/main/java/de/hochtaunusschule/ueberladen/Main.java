f6af8a3c5d99108ddd25d578bcf8e5fdc4f0e802
david_369 <davidscandurra@gmail.com>
2021-11-22 15:19:19 +0100

    Implement test suite in ueberladen

--- ueberladen/src/main/java/de/hochtaunusschule/ueberladen/Main.java
+++ ueberladen/src/main/java/de/hochtaunusschule/ueberladen/Main.java
@@ -1,15 +1,37 @@
 package de.hochtaunusschule.ueberladen;
 
+import de.hochtaunusschule.testsuite.TestSuite;
+import de.hochtaunusschule.testsuite.test.SimpleTest;
+import de.hochtaunusschule.testsuite.token.TokenStream;
 import java.io.File;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        for (File file : new File("ueberladen/src/main/resources/hotell/").listFiles()) {
-            System.out.println("-> " + file.getName());
-            try (HotelReader reader = new HotelReader(file)) {
-                PathFind pathFind = new PathFind(reader.readAll());
-                System.out.println("   " + pathFind.bestPath().format());
-            }
+public class Main extends SimpleTest<HotelRegistry> {
+    public static void main(String[] args) {
+        TestSuite.launch(Main::new);
+    }
+
+    protected Main(File testFile) {
+        super(testFile);
+    }
+
+    @Override
+    protected void test(HotelRegistry hotelRegistry) {
+        PathFind pathFind = new PathFind(hotelRegistry);
+        System.out.println("   " + pathFind.bestPath().format());
+    }
+
+    @Override
+    protected HotelRegistry convert(TokenStream tokenStream) {
+        HotelRegistry registry = new HotelRegistry();
+        int size = tokenStream.next().asInt();
+        int end = tokenStream.next().asInt();
+        registry.register(new Hotel(10_000, end));
+        for (int i = 0; i < size; i++) {
+            TokenStream lineStream = tokenStream.next().splitSpace();
+            registry.register(new Hotel(
+                lineStream.at(1).asDouble(),
+                lineStream.at(0).asInt()));
         }
+        return registry;
     }
 }
